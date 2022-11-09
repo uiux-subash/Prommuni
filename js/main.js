010ee40751f4195d43dcc6a847cd959761b1d548
Masuk Helal Anik <aniksharif@gmail.com>
2022-11-10 00:55:11 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+  
+  (function() {
+    "use strict";
+    /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+    /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    if (all) {
+      select(el, all).forEach(e => e.addEventListener(type, listener))
+    } else {
+      select(el, all).addEventListener(type, listener)
+    }
+  }
+
+    
+    /**
+   * Mobile nav toggle
+   */
+   on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+})();
